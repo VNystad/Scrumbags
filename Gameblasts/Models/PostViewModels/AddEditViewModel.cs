63c300b955b0eb8d8bec8532d20708d80e4f22e4
Vebjørn Nystad <vebjon15@student.uia.no>
2017-03-29 11:27:08 +0200

    Made a simple ViewModel for adding and editing posts

---
+++ Gameblasts/Models/PostViewModels/AddEditViewModel.cs
@@ -0,0 +1,16 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Gameblasts.Models.PstViewModels
+{
+    public class PostViewModel
+    {
+        [Required]
+        [Display(Name = "New Title")]
+        public string Title{get; set;}
+
+        [Required]
+        [Display(Name = "New Body")]
+        public string Body{get; set;}
+    }
+}
