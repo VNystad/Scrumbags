7782923015eb35f946f2a64dbdf81ef03bfcb039
Vebjørn Nystad <vebjon15@student.uia.no>
2017-03-29 11:15:39 +0200

    Made PostModel that includes subcategory(implemented later)

---
+++ Gameblasts/Models/Post.cs
@@ -0,0 +1,37 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Gameblasts.Models;
+
+namespace Gameblast.Models
+{
+    public class Post
+    {
+        public Post(ApplicationUser user, string title, string body/*, SubCategory subcat*/)
+        {
+            this.Title = title;
+            this.User = user;
+            this.Body = body;
+            this.Date = DateTime.Now;
+            /*this.SubCat = subcat */
+        }
+
+        [Required]
+        [KeyAttribute]
+        public int Id{get; set;}
+
+        [Required]
+        public string Title{get; set;}
+
+        [Required]
+        public ApplicationUser User{get; set;}
+
+        [Required]
+        public DateTime Date{get; set;}
+
+        [Required]
+        public string Body{get; set;}
+
+        //[Required]
+        //public SubCategory SubCat{get; set;}
+    }
+}
