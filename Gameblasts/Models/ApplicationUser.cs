5c382c0cb07fdd20608f945fdf42aed2f413f866
Vebjørn Nystad <vebjon15@student.uia.no>
2017-03-30 20:53:26 +0200

    ApplicationUser now has a collection of posts

--- Gameblasts/Models/ApplicationUser.cs
+++ Gameblasts/Models/ApplicationUser.cs
@@ -5,5 +5,7 @@ namespace Gameblasts.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+         public virtual ICollection<Post> Posts { get; set; }
+        
     }
 }
