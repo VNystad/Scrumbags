059ecb9e2a5a25b29599bb8ff416647c65ffe782
Vebjørn Nystad <vebjon15@student.uia.no>
2017-03-30 05:46:34 +0200

    Made a viewcomponent to get posts from db to a view

---
+++ Gameblasts/ViewComponents/PostViewComponent.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Gameblasts.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ViewComponents
+{
+    public class Post : ViewComponent
+    {
+        private ApplicationDbContext db;
+
+        public Post(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int count = 0)
+        {
+            return View(db.Posts.Include(p => p.User).Take(count).ToList());
+        }
+    }
+}
