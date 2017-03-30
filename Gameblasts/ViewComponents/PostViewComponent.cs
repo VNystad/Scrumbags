1a2d884fc072971016b4dbd90c60e46f8aeca024
Vebjørn Nystad <vebjon15@student.uia.no>
2017-03-30 10:29:38 +0200

    Made viewComponent to get posts from db.

--- Gameblasts/ViewComponents/PostViewComponent.cs
+++ Gameblasts/ViewComponents/PostViewComponent.cs
@@ -18,6 +18,7 @@ namespace ViewComponents
 
         public async Task<IViewComponentResult> InvokeAsync(int count = 0)
         {
+            /*This takes "count" of posts from database */
             return View(db.Posts.Include(p => p.User).Take(count).ToList());
         }
     }
