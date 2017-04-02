b98d283ab290314589c4a2479a6d6addabc61e32
Vegar Kristoffer Kvammen <Vegark16@uia.no>
2017-04-02 17:43:55 +0200

    Moved CategoryViewComponent to a more appropriate location, and edited ForumCategoryDemo to call which category to expand

--- Gameblasts/ViewComponents/CategoryViewComponent.cs
+++ Gameblasts/ViewComponents/CategoryViewComponent.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using CategoryModels;
 using Gameblasts.Data;
 using Microsoft.AspNetCore.Mvc;
@@ -18,22 +17,9 @@ namespace ViewComponents
 
         public async Task<IViewComponentResult> InvokeAsync(string topCatName)
         {
-            
-            //var topCat = db.topCategories.Where(s => s.name.Equals(topCatName));
-            
-            var topCat = await db.topCategories.FindAsync(topCatName);  
 
-/*
-            var topCat1 = new CategoryModels.TopCategoryModel(topCat.name, new List<CategoryModels.SubCategoryModel>());
-
-            var SubCat1 = new CategoryModels.SubCategoryModel("SubCat1", topCat1, null);
-            var SubCat2 = new CategoryModels.SubCategoryModel("SubCat1", topCat1, null);
-            var SubCat3 = new CategoryModels.SubCategoryModel("SubCat1", topCat1, null);
-            topCat1.children.Add(SubCat1);
-            topCat1.children.Add(SubCat2);
-            topCat1.children.Add(SubCat3);
-  */          
-            return View(topCat);
+            TopCategoryModel topCat = await db.topCategories.FindAsync(topCatName);
+            return View(topCat.children);
         }
     }
 }
