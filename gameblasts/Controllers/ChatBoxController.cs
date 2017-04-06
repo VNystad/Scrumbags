3592d868226968f3c0f13110d6844aea5ae716d0
Kristoffer Magelssen <krislm15@uia.no>
2017-04-06 19:47:26 +0200

    SB-39 Added a controller for the chatbox.

---
+++ gameblasts/Controllers/ChatBoxController.cs
@@ -0,0 +1,41 @@
+using Gameblasts.Models.PostViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Gameblasts.Data;
+using Microsoft.AspNetCore.Identity;
+using Gameblasts.Models;
+
+namespace Gameblasts.Controllers
+{
+    public class ChatBoxController : Controller
+    {   
+        private ApplicationDbContext ApplicationDbContext;
+        private UserManager<ApplicationUser> UserManager { get; set; }
+        private readonly SignInManager<ApplicationUser> SignInManager;
+        
+        public ChatBoxController(ApplicationDbContext db, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            this.ApplicationDbContext = db;
+            this.UserManager = userManager;
+            this.SignInManager = signInManager;
+        }
+
+        public async Task<IActionResult> AddChat(AddEditChatBoxViewModel vm, string message)
+        {   
+            ChatMessage newmessage = new ChatMessage(await GetCurrentUserAsync(),message);
+            ApplicationDbContext.ChatMessages.Add(newmessage);
+            ApplicationDbContext.SaveChanges();
+            return View("../Home/ChatBox");
+        }
+
+        public IActionResult ChatBox()
+        {
+            return View();
+        }
+
+        private Task<ApplicationUser> GetCurrentUserAsync()
+        {
+            return UserManager.GetUserAsync(HttpContext.User);
+        }
+    }
+}
