a7857ce74422442292d1b460e5c242c8ed464f30
Vebjørn Nystad <vebjon15@student.uia.no>
2017-05-21 01:32:47 +0200

    Made a controller for userprofile

---
+++ Gameblasts/Controllers/ProfileController.cs
@@ -0,0 +1,24 @@
+using Gameblasts.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gameblasts.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly string _externalCookieScheme;
+
+        public ProfileController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
