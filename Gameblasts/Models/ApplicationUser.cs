a770b5a9ad67754b002331854eb15a27ef33c9e6
Vebjørn Nystad <vebjon15@student.uia.no>
2017-05-20 12:05:04 +0200

    Made some fields required and removed some fields

--- Gameblasts/Models/ApplicationUser.cs
+++ Gameblasts/Models/ApplicationUser.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Gameblasts.Models
@@ -7,16 +8,17 @@ namespace Gameblasts.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        [Required]
         public string AboutInfo { get; set; }
+        [Required]
         public string MemberTitle { get; set; }
         public string Location { get; set; }
         public string Gender { get; set; }
         public string SocialMediaNames { get; set; }
         public int Age { get; set; }
+        [Required]
         public int PostCount { get; set; }
+        [Required]
         public DateTime RegisterDate { get; set; }
 
         public virtual ICollection<Post> Posts { get; set; }
