b206a1b2c5c35062c9319a3767e52dd4a269ca75
Vebjørn Nystad <vebjon15@student.uia.no>
2017-05-20 11:56:01 +0200

    Edited the applicationusermodel, has now more properties for our needs

--- Gameblasts/Models/ApplicationUser.cs
+++ Gameblasts/Models/ApplicationUser.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
@@ -6,7 +7,19 @@ namespace Gameblasts.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-         public virtual ICollection<Post> Posts { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AboutInfo { get; set; }
+        public string MemberTitle { get; set; }
+        public string Location { get; set; }
+        public string Gender { get; set; }
+        public string SocialMediaNames { get; set; }
+        public int Age { get; set; }
+        public int PostCount { get; set; }
+        public DateTime RegisterDate { get; set; }
+
+        public virtual ICollection<Post> Posts { get; set; }
         
     }
 }
