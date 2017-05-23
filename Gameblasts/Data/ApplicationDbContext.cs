8822a31aa2e5c7c491a706cbcc03020fd27c4899
Kristoffer Magelssen <krislm15@uia.no>
2017-05-23 15:10:28 +0200

    SB-81 Just some minor tweaks.

--- Gameblasts/Data/ApplicationDbContext.cs
+++ Gameblasts/Data/ApplicationDbContext.cs
@@ -7,10 +7,7 @@ namespace Gameblasts.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<Post> Posts { get; set; }
 
