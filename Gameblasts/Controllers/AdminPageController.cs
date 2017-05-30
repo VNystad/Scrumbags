71ceb990956684c577740d98540f9dc5030ca7af
Martin Bråten <martib15@student.uia.no>
2017-05-30 10:52:47 +0200

    SB-60 Fixed AdminPage. Affected files: AdminPage Controller, Model and View. It now works and it also removes all other roles of the user before adding the role to the user. There is still more things that could be better such as removing current roles in one statement instead of one if-statement for each role and feedback to the view.

--- Gameblasts/Controllers/AdminPageController.cs
+++ Gameblasts/Controllers/AdminPageController.cs
@@ -6,7 +6,8 @@ using static Gameblasts.Controllers.ManageController;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Threading.Tasks;
+using Gameblasts.Models.AdminPageViewModels;
 namespace Gameblasts.Controllers
 {
     public class AdminPageController : Controller
@@ -17,73 +18,36 @@ namespace Gameblasts.Controllers
         {
             _userManager = um;
         }
-/*
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public virtual ActionResult Edit(Microsoft.AspNetCore.Identity.UserLoginInfo user, string role)
-        {
-            if (ModelState.IsValid)
-            {
-                // THIS LINE IS IMPORTANT
-                var oldUser = Microsoft.AspNetCore.Identity.UserManager.FindById(user);
-                var oldRoleId = oldUser.Roles.SingleOrDefault().RoleId;
-                var oldRoleName = DB.Roles.SingleOrDefault(r => r.Id == oldRoleId).Name;
-
-                if (oldRoleName != role)
-                {
-                    Manager.RemoveFromRole(user.Id, oldRoleName);
-                    Manager.AddToRole(user.Id, role);
-                }
-                DB.Entry(user).State = EntityState.Modified;
-
-                return RedirectToAction(MVC.User.Index());
-            }
-            return View(user);
-        }
-*/
-
-/*        [HttpPost]
-        [ValidateAntiForgeryToken] */
-
         [Authorize(Roles="Admin")]
         public IActionResult AdminPage()
         {
             return View();
         }
-
-        //Burde vært bool check, men får ike dette til per nå.
-        public async void AddUserToRole(string email, string rolename)
+        [HttpPost]
+        public async Task<IActionResult> AddUserToRole(AdminPageChangeRoleViewModel Info)
         {
-            //var userManager = serviceScope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
-            var user = await _userManager.FindByEmailAsync(email);
+            var user = await _userManager.FindByEmailAsync(Info.Email);
             if(user == null)
             {
-                return;
+                return View("AdminPage");
             }
             else
             {
-                await _userManager.AddToRoleAsync(user, rolename);
-                return;
+                if(await _userManager.IsInRoleAsync(user, "Member"))
+                {
+                    await _userManager.RemoveFromRoleAsync(user, "Member");
+                }
+                if(await _userManager.IsInRoleAsync(user, "Moderator"))
+                {
+                    await _userManager.RemoveFromRoleAsync(user, "Moderator");
+                }
+                if(await _userManager.IsInRoleAsync(user, "Admin"))
+                {
+                    await _userManager.RemoveFromRoleAsync(user, "Admin");
+                }
+                await _userManager.AddToRoleAsync(user, Info.Role);
+                return View("AdminPage");
             }
         }
-
-
-/*
-                    var user = await userManager.FindByNameAsync("admin@uia.no");
-            if(user == null)
-            {
-                // Then add one admin user if it doesn't already exists.
-                var adminUser = new ApplicationUser( "admin@uia.no", "admin@uia.no" );
-                await userManager.CreateAsync(adminUser, "Password1.");
-                await userManager.AddToRoleAsync(adminUser, "Admin");
-            }
-*/
-/* 
-        public bool AddUserToRole(string userId, string roleName)
-        {
-            var idResult = _userManager.AddToRole(userId, roleName);
-            return idResult.Succeeded;
-        }
-*/
     }
 }
