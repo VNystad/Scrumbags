fcd69082e7991242c57947fe156c382b96cd9805
Martin Bråten <martib15@student.uia.no>
2017-05-31 09:54:56 +0200

    SB-83 Added comments on AdminPage view, AdminPageViewModel and AdminPageController. Also added authorize for admin on AdminPageController post function

--- Gameblasts/Controllers/AdminPageController.cs
+++ Gameblasts/Controllers/AdminPageController.cs
@@ -13,7 +13,12 @@ namespace Gameblasts.Controllers
     public class AdminPageController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
-
+        
+/**This is the controller of the AdminPage. Its task is to actually perform the action in the AdminPage.
+*The link to the AdminPage is only shown for users in the Admin role (done in _LoginPartial.cshtml) and other users are also blocked from accessing this page and posting to it.
+*The way the AdminPageController works is that it receives the email-address and new role of the user whom should have a change of role.
+*Then the controller finds the id of the user based on its email-address, removes any roles it may have and then assigns it the new role sent fromt the View/ViewModel.
+*/
         public AdminPageController(UserManager<ApplicationUser> um) 
         {
             _userManager = um;
@@ -24,6 +29,7 @@ namespace Gameblasts.Controllers
             return View();
         }
         [HttpPost]
+        [Authorize(Roles="Admin")]
         public async Task<IActionResult> AddUserToRole(AdminPageChangeRoleViewModel Info)
         {
             var user = await _userManager.FindByEmailAsync(Info.Email);
--- Gameblasts/Models/AdminPageViewModels/AdminPageChangeRoleViewModel.cs
+++ Gameblasts/Models/AdminPageViewModels/AdminPageChangeRoleViewModel.cs
@@ -1,7 +1,11 @@
+
 using System;
 
 namespace Gameblasts.Models.AdminPageViewModels
 {
+    ///This is the view model of the AdminPage. 
+    ///This view model specifies which data from the AdminPage view to be sent to the AdminPage controller, 
+    //which simply specifies that all variables are to be sent (email, new role).
     public class AdminPageChangeRoleViewModel
     {
         public string Email { get; set; }
